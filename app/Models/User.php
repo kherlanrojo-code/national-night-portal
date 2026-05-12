2978adf2b760e0b5865de9d90191d6d91949863e
kherlanrojo-code <kherlanrojo@gmail.com>
2026-05-12 14:49:22 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes; // Add this line
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    use SoftDeletes; // Add this line inside the class
 
     /**
      * The attributes that are mass assignable.
